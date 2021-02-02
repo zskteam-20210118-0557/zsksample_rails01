72ee99b0e6853cc7600aee31c95de5872e7a0f5b
shinora008 <k.shinozaki8@gmail.com>
2021-02-02 20:06:38 +0900

    feat: micropost test

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -1,2 +1,4 @@
 class Micropost < ApplicationRecord
+  validates :content, presence: true
+  validates :user_id, presence: true
 end
--- spec/models/micropost_spec.rb
+++ spec/models/micropost_spec.rb
@@ -1,5 +1,23 @@
 require 'rails_helper'
 
 RSpec.describe Micropost, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:micropost) { create(:micropost) }
+
+  context 'バリデーション' do
+    it 'マイクロポストが有効である' do
+      expect(micropost).to be_valid
+    end
+
+    it 'コンテントがない場合は無効である' do
+      micropost = build(:micropost, content: '    ')
+      micropost.valid?
+      expect(micropost.errors[:content]).to include("can't be blank")
+    end
+
+    it 'ユーザーがない場合は無効である' do
+      micropost = build(:micropost, user_id: nil)
+      micropost.valid?
+      expect(micropost.errors[:user_id]).to include("can't be blank")
+    end
+  end
 end
